a7031aa956fd78aec29760411810ca20fbdf5185
Payal-Exf <payalkulshreshtha@exafluence.com>
2024-05-28 12:54:46 +0530

    main file

---
+++ Backend/SCMXPertLite_Backend.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from routes.route import router
+
+app = FastAPI()
+
+origins = ['https://localhost:8000',
+           'http://localhost:8000',
+           'http://localhost:8080',
+           'http://127.0.0.1:8080',
+           'https://127.0.0.1:8080']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(router)
