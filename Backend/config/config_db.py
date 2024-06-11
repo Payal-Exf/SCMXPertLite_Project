1369e08b5c668a1833f1c96ca66c9596b42009c4
Payal-Exf <payalkulshreshtha@exafluence.com>
2024-06-11 18:15:01 +0530

    MongoDb connection configuration changed to meed docker needs

--- Backend/config/config_db.py
+++ Backend/config/config_db.py
@@ -1,6 +1,9 @@
+import os
 from pymongo.mongo_client import MongoClient
+from pymongo import errors
 
-url = "mongodb://127.0.0.1:27017"
+url = os.getenv("MONGO_URL","mongodb://mongo:27017/")
+#url = "mongodb://127.0.0.1:27017/"
 
 #Connecting to MongoDb
 client = MongoClient(url)
@@ -8,8 +11,15 @@ client = MongoClient(url)
 #Using SCMPertLite_db as current database
 mydb = client['SCMXPertLite_db']
 
-# Create Collections if not exists -- Users & Shipments
 user = mydb.Users
 shipment = mydb.Shipments
 device = mydb.Devices
 
+try:
+    # Attempt to retrieve server information
+    client.server_info()
+    print("Connected to MongoDB")
+except errors.ServerSelectionTimeoutError as err:
+    print(f"Server selection error: {err}")
+except errors.ConnectionFailure as err:
+    print(f"Connection failure: {err}")
